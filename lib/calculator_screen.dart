73805867d784cbce0360f74f1ff5c69e17b4d79a
irham maulana <i.maulana.azn@gmail.com>
2024-06-28 20:42:59 +0700

    calculator finished

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+import 'package:vibration/vibration.dart';
 import 'options_screen.dart';
+import 'themes.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -12,167 +16,273 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   String userInput = "";
   String result = "0";
+  String selectedTheme = 'black';
+  bool isDarkMode = false;
+  bool isVibrationOn = false;
 
-  List<String> buttonList = [
-    "AC",
-    "(",
-    ")",
-    "/",
-    "7",
-    "8",
-    "9",
-    "*",
-    "4",
-    "5",
-    "6",
-    "+",
-    "1",
-    "2",
-    "3",
-    "-",
-    "%",
-    "0",
-    ".",
-    "=",
-    "C",
+  List<List<String>> buttonList = [
+    [
+      "(",
+      ")",
+      "%",
+      "/",
+    ],
+    [
+      "7",
+      "8",
+      "9",
+      "*",
+    ],
+    [
+      "4",
+      "5",
+      "6",
+      "-",
+    ],
+    [
+      "1",
+      "2",
+      "3",
+      "+",
+    ],
+    [
+      ".",
+      "0",
+      "=",
+    ]
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF1d2630),
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 3,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.settings, color: Colors.white),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => OptionsScreen()),
-                    );
-                  },
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
+    return Theme(
+      data: isDarkMode ? ThemeData.dark() : AppThemes.themes[selectedTheme]!,
+      child: Scaffold(
+        backgroundColor: isDarkMode
+            ? ThemeData.dark().scaffoldBackgroundColor
+            : AppThemes.themes[selectedTheme]!.scaffoldBackgroundColor,
+        body: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 3,
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            userInput,
+                            style: TextStyle(
+                                fontSize: 48,
+                                color: isDarkMode
+                                    ? ThemeData.dark()
+                                        .textTheme
+                                        .bodyText1!
+                                        .color
+                                    : Colors.white),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            result,
+                            style: TextStyle(
+                                fontSize: 26,
+                                color: isDarkMode
+                                    ? ThemeData.dark()
+                                        .textTheme
+                                        .bodyText1!
+                                        .color
+                                    : Colors.white30),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
                     children: [
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          userInput,
-                          style: TextStyle(fontSize: 32, color: Colors.white),
-                        ),
+                      IconButton(
+                        icon: Icon(FontAwesomeIcons.gear,
+                            size: 30.0,
+                            color: isDarkMode
+                                ? ThemeData.dark().iconTheme.color
+                                : AppThemes
+                                    .themes[selectedTheme]!.iconTheme.color),
+                        onPressed: () async {
+                          final result = await Navigator.push(
+                            context,
+                            _createRoute(OptionsScreen(
+                              isDarkMode: isDarkMode,
+                              isVibrationOn: isVibrationOn,
+                              selectedTheme: selectedTheme,
+                            )),
+                          );
+                          if (result != null && result is Map) {
+                            setState(() {
+                              isDarkMode = result['isDarkMode'];
+                              isVibrationOn = result['isVibrationOn'];
+                              selectedTheme = result['selectedTheme'];
+                            });
+                          }
+                        },
                       ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          result,
-                          style: TextStyle(
-                              fontSize: 48,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
+                      IconButton(
+                        icon: Icon(Icons.delete_sharp,
+                            color: isDarkMode
+                                ? ThemeData.dark().iconTheme.color
+                                : AppThemes
+                                    .themes[selectedTheme]!.iconTheme.color),
+                        onPressed: () {
+                          handleClearAllButtons();
+                        },
                       ),
+                      Expanded(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            alignment: Alignment.centerRight,
+                            padding: const EdgeInsets.only(right: 10),
+                            child: IconButton(
+                              icon: Icon(Icons.backspace,
+                                  color: isDarkMode
+                                      ? ThemeData.dark().iconTheme.color
+                                      : AppThemes.themes[selectedTheme]!
+                                          .iconTheme.color),
+                              onPressed: () {
+                                handleBackspaceButtons();
+                              },
+                            ),
+                          ),
+                        ],
+                      ))
                     ],
                   ),
-                ),
-              ],
-            ),
-          ),
-          Divider(color: Colors.white),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: GridView.builder(
-                itemCount: buttonList.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12),
-                itemBuilder: (BuildContext context, int index) {
-                  return CustomButton(buttonList[index]);
-                },
+                ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget CustomButton(String text) {
-    return InkWell(
-      splashColor: Color(0xFF1d2630),
-      onTap: () {
-        setState(() {
-          handleButtons(text);
-        });
-      },
-      child: Ink(
-        decoration: BoxDecoration(
-          color: getBgColor(text),
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.white.withOpacity(0.1),
-                blurRadius: 4,
-                spreadRadius: 0.5,
-                offset: Offset(-3, -3))
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(top: 15),
+                child: Column(
+                  children: buttonList.map((row) {
+                    return Expanded(
+                      child: Row(
+                        children: row.map((buttonText) {
+                          // Check if button should span two columns
+                          if (buttonText == "=") {
+                            return Expanded(
+                              flex: 2, // Span two columns
+                              child: CustomButton(
+                                text: buttonText,
+                                isVibrationOn:
+                                    isVibrationOn, // Example variable
+                                themeData: isDarkMode
+                                    ? ThemeData.dark()
+                                    : AppThemes.themes[selectedTheme]!,
+                              ),
+                            );
+                          }
+                          return Expanded(
+                            child: CustomButton(
+                              text: buttonText,
+                              isVibrationOn: isVibrationOn, // Example variable
+                              themeData: isDarkMode
+                                  ? ThemeData.dark()
+                                  : AppThemes.themes[selectedTheme]!,
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
           ],
         ),
-        child: Center(
-          child: Text(text,
-              style: TextStyle(
-                  color: getColor(text),
+      ),
+    );
+  }
+
+  Widget CustomButton(
+      {required String text,
+      required bool isVibrationOn,
+      required ThemeData themeData}) {
+    return Container(
+      margin: const EdgeInsets.all(2),
+      child: InkWell(
+        splashColor: const Color(0xFF1d2630),
+        onTap: () {
+          setState(() {
+            if (isVibrationOn) {
+              Vibration.vibrate(duration: 50);
+            }
+            handleButtons(text);
+          });
+        },
+        child: Ink(
+          decoration: BoxDecoration(
+            color: getBgColor(text, themeData),
+            borderRadius: BorderRadius.circular(3),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.15),
+                  blurRadius: 1,
+                  spreadRadius: 0.2,
+                  offset: const Offset(0, 4))
+            ],
+          ),
+          child: Center(
+            child: Text(
+                text == "*"
+                    ? "x"
+                    : text == "/"
+                        ? "÷"
+                        : text,
+                style: TextStyle(
+                  color: getTextColor(text, themeData),
                   fontSize: 30,
-                  fontWeight: FontWeight.bold)),
+                )),
+          ),
         ),
       ),
     );
   }
 
-  getColor(String text) {
-    if (text == "/" ||
-        text == "*" ||
-        text == "+" ||
-        text == "-" ||
-        text == "%" ||
-        text == "C" ||
-        text == "(" ||
-        text == ")") {
-      return Color.fromARGB(255, 252, 100, 100);
+  Color getTextColor(String text, ThemeData themeData) {
+    if (text == "=") {
+      return themeData.colorScheme.onSecondary;
     }
-    return Colors.white;
+    if (["/", "*", "-", "+", "%", "(", ")"].contains(text)) {
+      return themeData.colorScheme.onTertiary;
+    }
+    return themeData.textTheme.bodyText1!.color!;
   }
 
-  getBgColor(String text) {
-    if (text == "AC") {
-      return Color.fromARGB(255, 252, 100, 100);
-    }
-
+  Color getBgColor(String text, ThemeData themeData) {
     if (text == "=") {
-      return Color.fromARGB(255, 104, 204, 159);
+      return themeData.colorScheme.secondary;
     }
-    return Color(0xFF1d2630);
+    if (["/", "*", "-", "+", "%", "(", ")"].contains(text)) {
+      return themeData.colorScheme.tertiary;
+    }
+    return themeData.primaryColor;
   }
 
   handleButtons(String text) {
-    if (text == "AC") {
-      userInput = "";
-      result = "0";
-    } else if (text == "C") {
-      if (userInput.isNotEmpty) {
-        userInput = userInput.substring(0, userInput.length - 1);
-      }
-    } else if (text == "=") {
+    if (text == "=") {
       result = calculate();
       userInput = result;
+      result = "0";
 
       if (userInput.endsWith(".0")) {
         userInput = userInput.replaceAll(".0", "");
@@ -185,6 +295,33 @@ class _CalculatorState extends State<Calculator> {
       }
     } else {
       userInput = userInput + text;
+      result = evaluateExpression(userInput);
+    }
+  }
+
+  handleClearAllButtons() {
+    setState(() {
+      userInput = "";
+      result = "0";
+    });
+  }
+
+  handleBackspaceButtons() {
+    setState(() {
+      if (userInput.isNotEmpty) {
+        userInput = userInput.substring(0, userInput.length - 1);
+        result = evaluateExpression(userInput);
+      }
+    });
+  }
+
+  String evaluateExpression(String input) {
+    try {
+      var exp = Parser().parse(input);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "please use correct expression";
     }
   }
 
@@ -196,5 +333,24 @@ class _CalculatorState extends State<Calculator> {
     } catch (e) {
       return "Error";
     }
+  }
+
+  Route _createRoute(Widget screen) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => screen,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
   }
 }
--- lib/options_screen.dart
+++ lib/options_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'themes.dart';
 
 class OptionsScreen extends StatefulWidget {
+  final bool isDarkMode;
+  final bool isVibrationOn;
+  final String selectedTheme;
+
+  OptionsScreen({
+    required this.isDarkMode,
+    required this.isVibrationOn,
+    required this.selectedTheme,
+  });
+
   @override
   _OptionsScreenState createState() => _OptionsScreenState();
 }
 
 class _OptionsScreenState extends State<OptionsScreen> {
-  bool isDarkMode = false;
-  bool isVibrationOn = false;
-  String selectedLanguage = 'English';
+  late bool isDarkMode;
+  late bool isVibrationOn;
+  late String selectedTheme;
   List<String> languages = ['English', 'Spanish', 'French', 'German'];
+  String selectedLanguage = 'English';
+
+  @override
+  void initState() {
+    super.initState();
+    isDarkMode = widget.isDarkMode;
+    isVibrationOn = widget.isVibrationOn;
+    selectedTheme = widget.selectedTheme;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,41 +37,70 @@ class _OptionsScreenState extends State<OptionsScreen> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Options'),
+          backgroundColor: Colors.black87,
+          title: DefaultTextStyle(
+            style: TextStyle(color: Colors.white, fontSize: 16),
+            child: Text('Options'),
+          ),
           bottom: TabBar(
+            indicatorColor: Colors.transparent,
+            unselectedLabelColor: Colors.white,
             tabs: [
               Tab(text: 'Options'),
               Tab(text: 'Themes'),
               Tab(text: 'About'),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            OptionsTab(
-              isDarkMode: isDarkMode,
-              isVibrationOn: isVibrationOn,
-              selectedLanguage: selectedLanguage,
-              languages: languages,
-              onDarkModeChanged: (value) {
-                setState(() {
-                  isDarkMode = value;
-                });
-              },
-              onVibrationChanged: (value) {
-                setState(() {
-                  isVibrationOn = value;
-                });
-              },
-              onLanguageChanged: (value) {
-                setState(() {
-                  selectedLanguage = value!;
-                });
-              },
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white, // set the color to white
             ),
-            ThemesTab(),
-            AboutTab(),
-          ],
+            onPressed: () {
+              Navigator.pop(context, {
+                'isDarkMode': isDarkMode,
+                'isVibrationOn': isVibrationOn,
+                'selectedTheme': selectedTheme,
+              });
+            },
+          ),
+        ),
+        body: Container(
+          color: Colors.black87,
+          child: TabBarView(
+            children: [
+              OptionsTab(
+                isDarkMode: isDarkMode,
+                isVibrationOn: isVibrationOn,
+                selectedLanguage: selectedLanguage,
+                languages: languages,
+                onDarkModeChanged: (value) {
+                  setState(() {
+                    isDarkMode = value;
+                  });
+                },
+                onVibrationChanged: (value) {
+                  setState(() {
+                    isVibrationOn = value;
+                  });
+                },
+                onLanguageChanged: (value) {
+                  setState(() {
+                    selectedLanguage = value!;
+                  });
+                },
+              ),
+              ThemesTab(
+                selectedTheme: selectedTheme,
+                onThemeChanged: (value) {
+                  setState(() {
+                    selectedTheme = value;
+                  });
+                },
+              ),
+              AboutTab(),
+            ],
+          ),
         ),
       ),
     );
@@ -85,26 +134,42 @@ class OptionsTab extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SwitchListTile(
-            title: Text('Dark Mode'),
+            title: Text('Dark Mode', style: TextStyle(color: Colors.white)),
             value: isDarkMode,
             onChanged: onDarkModeChanged,
           ),
           SwitchListTile(
-            title: Text('Vibrations'),
+            title: Text('Vibrations', style: TextStyle(color: Colors.white)),
             value: isVibrationOn,
             onChanged: onVibrationChanged,
           ),
-          DropdownButtonFormField<String>(
-            value: selectedLanguage,
-            items: languages.map((String language) {
-              return DropdownMenuItem<String>(
-                value: language,
-                child: Text(language),
-              );
-            }).toList(),
-            onChanged: onLanguageChanged,
-            decoration: InputDecoration(
-              labelText: 'Select Language',
+          Theme(
+            data: Theme.of(context).copyWith(
+              canvasColor: Colors.black54, // Background color of the dropdown
+            ),
+            child: DropdownButtonFormField<String>(
+              value: selectedLanguage,
+              items: languages.map((String language) {
+                return DropdownMenuItem<String>(
+                  value: language,
+                  child: Text(language, style: TextStyle(color: Colors.white)),
+                );
+              }).toList(),
+              onChanged: onLanguageChanged,
+              dropdownColor: Color.fromARGB(
+                  255, 55, 55, 55), // Background color for the dropdown
+              decoration: InputDecoration(
+                labelText: 'Language',
+                labelStyle:
+                    TextStyle(color: Colors.white), // Set color to white
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white), // Border color
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.white), // Focused border color
+                ),
+              ),
             ),
           ),
         ],
@@ -114,10 +179,128 @@ class OptionsTab extends StatelessWidget {
 }
 
 class ThemesTab extends StatelessWidget {
+  final String selectedTheme;
+  final ValueChanged<String> onThemeChanged;
+
+  ThemesTab({required this.selectedTheme, required this.onThemeChanged});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Themes Tab'),
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: AppThemes.themes.keys.map((String theme) {
+                final ThemeData themeData = AppThemes.themes[theme]!;
+
+                return ListTile(
+                  leading: Icon(
+                    Icons.palette,
+                    color: themeData.colorScheme
+                        .secondary, // Use the theme's primary color for the icon
+                  ),
+                  title: Text(
+                    theme,
+                    style: TextStyle(
+                        color: Colors.white), // Set title text color to white
+                  ),
+                  trailing: selectedTheme == theme
+                      ? Icon(Icons.check, color: Colors.green)
+                      : null,
+                  onTap: () {
+                    onThemeChanged(theme);
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(height: 10),
+          CalculatorPreview(selectedTheme: selectedTheme),
+        ],
+      ),
+    );
+  }
+}
+
+class CalculatorPreview extends StatelessWidget {
+  final String selectedTheme;
+
+  CalculatorPreview({required this.selectedTheme});
+
+  Color getBgColor(String text, ThemeData themeData) {
+    if (text == "=") {
+      return themeData.colorScheme.secondary;
+    }
+    if (["/", "*", "-", "+", "%", "(", ")"].contains(text)) {
+      return themeData.colorScheme.tertiary;
+    }
+    return themeData.primaryColor;
+  }
+
+  Color getTextColor(String text, ThemeData themeData) {
+    if (text == "=") {
+      return themeData.colorScheme.onSecondary;
+    }
+    if (["/", "*", "-", "+", "%", "(", ")"].contains(text)) {
+      return themeData.colorScheme.onTertiary;
+    }
+    return themeData.textTheme.bodyText1?.color ?? Colors.white;
+  }
+
+  Widget CustomButton({
+    required String text,
+    required bool isVibrationOn,
+    required ThemeData themeData,
+  }) {
+    return Expanded(
+      flex: text == "=" ? 2 : 1,
+      child: Container(
+        height: 50.0, // Adjust as needed
+        decoration: BoxDecoration(
+          color: getBgColor(text, themeData),
+          borderRadius: BorderRadius.all(
+            Radius.circular(3),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            '$text',
+            style: TextStyle(
+              color: getTextColor(text, themeData),
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = AppThemes.themes[selectedTheme]!;
+
+    return Theme(
+      data: themeData,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CustomButton(text: "+", isVibrationOn: true, themeData: themeData),
+            SizedBox(width: 4),
+            CustomButton(text: "1", isVibrationOn: true, themeData: themeData),
+            SizedBox(width: 4),
+            CustomButton(text: "2", isVibrationOn: true, themeData: themeData),
+            SizedBox(width: 4),
+            CustomButton(text: "3", isVibrationOn: true, themeData: themeData),
+            SizedBox(width: 4),
+            CustomButton(text: "=", isVibrationOn: true, themeData: themeData),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -125,8 +308,53 @@ class ThemesTab extends StatelessWidget {
 class AboutTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('About Tab'),
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                "About",
+                style: TextStyle(color: Colors.white38, fontSize: 12),
+              ),
+            ],
+          ),
+          ListTile(
+            leading: Icon(Icons.info_outline, color: Colors.blue),
+            title: Text(
+              'Version',
+              style: TextStyle(
+                  color: Colors.white), // Set title text color to white
+            ),
+            subtitle: Text(
+              '1.09',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white60), // Set subtitle text color to white
+            ),
+            onTap: () {},
+          ),
+          Divider(
+            color: Colors.black,
+          ),
+          ListTile(
+            leading: Icon(Icons.info_outline, color: Colors.blue),
+            title: Text(
+              'For more information, please visit',
+              style: TextStyle(
+                  color: Colors.white), // Set title text color to white
+            ),
+            subtitle: Text(
+              'FOTHONG.COM',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white60), // Set subtitle text color to white
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
